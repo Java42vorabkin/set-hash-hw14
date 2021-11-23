74e08397d4ec772bffb198ac96b74979e10877a1
vorabkin <vorabkin@gmail.com>
2021-11-23 20:46:12 +0200

    adds HashSetIterator implementation

--- src/telran/util/HashSet.java
+++ src/telran/util/HashSet.java
@@ -84,25 +84,79 @@ public class HashSet<T> extends AbstractSet<T> {
 	@Override
 	public Iterator<T> iterator() {
 		//TODO 
-		//return new HashSetIterator();
-		return null;
+		return new HashSetIterator<T>();
 	}
-	private class HashSetIterator implements Iterator<T> {
+	private class HashSetIterator<T> implements Iterator<T> {
 //TODO iterator required fields
+		// Each of backeIterators saves the backet iterator
+		Iterator<T> backeIterators[];
+		int curBacketIndex = -1 ;
+		int prevBacketIndex;
+		// Done
+		
+		// Constructor
+		public HashSetIterator() {
+			backeIterators = new Iterator[hashTable.length];
+			for(int i=0; i < hashTable.length; i++) {
+				if(hashTable[i]!= null) {
+					// Filling element backeIterators by backet iterator
+					backeIterators[i] = (Iterator<T>) hashTable[i].iterator(); 
+					if(curBacketIndex<0) { 
+						// Saving the index of first non-null hashTable element
+						curBacketIndex = i;
+					}
+				}	
+			}
+		}
+		
 		@Override
 		public boolean hasNext() {
 			// TODO Auto-generated method stub
-			return false;
+			return curBacketIndex < backeIterators.length && backeIterators[curBacketIndex].hasNext();
+			// Done
 		}
 
 		@Override
 		public T next() {
 			// TODO Auto-generated method stub
-			return null;
+			T res = backeIterators[curBacketIndex].next();
+			prevBacketIndex = curBacketIndex;
+			curBacketIndex = getNextCurrentIndex(curBacketIndex);
+			return res;
+			// Done
 		}
 		@Override
 		public void remove() {
 			//TODO
+			backeIterators[prevBacketIndex].remove();
+			size--;
+			// Done
+		}
+		
+		private int getNextCurrentIndex(int curIndex) {
+			// Checking the current backet.
+			if(	!backeIterators[curIndex].hasNext()) {
+				// There aren't available elements in the current backet
+				while(++curIndex < backeIterators.length && 
+						(isLinkListNotCreated(curIndex) || isLinkedListPassed(curIndex))) {
+				}
+				/* 1. non-null backetIterator isn't found, curIndex is increased
+				 * 2. backetIterator is found and its index is returned
+				 */
+			} else {
+				// For comments only. Current backet isn't empty yet.
+				// curIndex isn't changed
+			}
+			// Index of the first non-null backet iterator is found
+			return curIndex;
+		}
+		private boolean isLinkListNotCreated(int index) {
+			// backet Iterator isn't created yet
+			return backeIterators[index]==null;
+		}
+		private boolean isLinkedListPassed(int index) {
+			// There isn't non-passed elements in the backet
+			return !backeIterators[index].hasNext();
 		}
 		
 	}
